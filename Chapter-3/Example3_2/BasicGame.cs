54fa1b7b9ea4c79a1fcd013f171d9ba8e656c683
Mitch Merrell <themitchguy@gmail.com>
2026-03-12 21:32:55 -0700

    create example 3-2

---
+++ Chapter-3/Example3_2/BasicGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Example3_2
+{
+    internal class BasicGame : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        [STAThread]
+        static void Main()
+        {
+            BasicGame game = new BasicGame();
+            game.Run();
+        }
+
+        public BasicGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Olive);
+        }
+    }
+}
